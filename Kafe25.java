14fbe3bf0c4da85d61a395855f852c508ce3f2d3
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-09-12 12:01:58 +0800

    commit nomor 3 dan 4

--- Kafe25.java
+++ Kafe25.java
@@ -3,7 +3,7 @@ public class Kafe25 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean keanggotaan;
-        int jmlKopi, jmlTeh, jmlRoti;
+        int jmlKopi, jmlTeh, jmlRoti, nominalint;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, nominalbayar;
         float diskon = 10 / 100f;
         byte totalByte;
@@ -16,7 +16,11 @@ public class Kafe25 {
         System.out.print("Masukkan jumlah pembelian roti: ");
         jmlRoti = input.nextInt();
         totalHarga = (jmlKopi*hargaKopi) + (jmlTeh*hargaTeh) + (jmlRoti*hargaRoti);
+        totalByte = (byte) totalHarga;
+        System.out.println("Total Byte: " + totalByte);
         nominalbayar = totalHarga - (diskon*totalHarga);
+        nominalint = (int) nominalbayar;
+        System.out.println("Nominal Integer: " + nominalint);
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
         System.out.println("Nominal bayar Rp" + nominalbayar);
