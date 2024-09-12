89cbb06216ddf68ba911c0bcb69639abeacf6d33
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-09-12 11:01:47 +0800

    commit percobaan 1

---
+++ Siakad25.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Siakad25 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama,nim;
+        String kelas;
+        char absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        System.out.print("Masukkan nama: ");
+        nama = sc.nextLine();
+        System.out.print("Masukkan NIM: ");
+        nim = sc.nextLine();
+        System.out.print("Masukkan kelas: ");
+        kelas = sc.nextLine();
+        System.out.print("Masukkan nomor absen: ");
+        absen = sc.nextLine().charAt(0);
+        System.out.print("Masukkan nilai kuis: ");
+        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukkan nilai tugas: ");
+        nilaiTugas = sc.nextDouble();
+        System.out.print("Masukkan nilai Ujian: ");
+        nilaiUjian = sc.nextDouble();
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) /3;
+        System.out.println("Nama: " + nama + " NIM: " + nim);
+        System.out.println("Kelas: " + kelas + " Absen: " + absen);
+        System.out.println("Nilai AKhir: " + nilaiAkhir);
+        
+    }
+}
